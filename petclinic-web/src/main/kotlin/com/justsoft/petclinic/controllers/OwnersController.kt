4a820e039b7dd8bb69bb7de45c8a8ca0b5603789
Max <rudko.max10@gmail.com>
2020-08-14 02:55:57 +0300

    OwnerController now lists owners on the index page. Closes #6

--- petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/OwnersController.kt
+++ petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/OwnersController.kt
@@ -1,14 +1,21 @@
 package com.justsoft.petclinic.controllers
 
+import com.justsoft.petclinic.services.OwnerService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/owners")
-class OwnersController {
+class OwnersController(
+        private val ownerService: OwnerService
+) {
 
     @RequestMapping("", "/", "/index", "/index.html")
-    fun listOwners(): String {
+    fun listOwners(model: Model): String {
+
+        model.addAttribute("owners", ownerService.findAll())
+
         return "owners/index"
     }
 }
