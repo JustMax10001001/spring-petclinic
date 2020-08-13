1626b7de3adb237c5c7c93bed9aac6bad8e82b16
Max <rudko.max10@gmail.com>
2020-08-13 21:32:24 +0300

    Made BaseEntity Serializable

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/BaseEntity.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/BaseEntity.kt
@@ -1,11 +1,11 @@
 package com.justsoft.petclinic.models
 
+import java.io.Serializable
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
-
 
 open class BaseEntity(
         @Id
         @GeneratedValue
         val id: Long? = null
-)
+): Serializable
