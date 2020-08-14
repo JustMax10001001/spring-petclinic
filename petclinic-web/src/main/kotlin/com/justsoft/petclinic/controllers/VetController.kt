a9b1e33f8eeee3b23555419af7d6f2b7cae124f8
Max <rudko.max10@gmail.com>
2020-08-14 03:02:53 +0300

    List Vets on their index page. Closes #5

--- petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/VetController.kt
+++ petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/VetController.kt
@@ -1,13 +1,21 @@
 package com.justsoft.petclinic.controllers
 
+import com.justsoft.petclinic.services.VetService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class VetController {
+@RequestMapping("/vets")
+class VetController(
+        private val vetService: VetService
+) {
 
-    @RequestMapping("/vets", "/vets/index", "/vets/index.html")
-    fun listVets(): String{
+    @RequestMapping("", "/", "/index", "/index.html")
+    fun listVets(model: Model): String {
+
+        model.addAttribute("vets", vetService.findAll())
+
         return "vets/index"
     }
 }
