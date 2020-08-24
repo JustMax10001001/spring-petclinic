0f39fda7e1e7daaa5a7708985a4adaaad0ec4455
Max <rudko.max10@gmail.com>
2020-08-24 12:48:40 +0300

    Refactoring

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/VetService.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/VetService.kt
@@ -1,6 +1,6 @@
 package com.justsoft.petclinic.services
 
-import com.justsoft.petclinic.models.people.vet.Vet
+import com.justsoft.petclinic.models.people.Vet
 import org.springframework.stereotype.Service
 
 @Service
