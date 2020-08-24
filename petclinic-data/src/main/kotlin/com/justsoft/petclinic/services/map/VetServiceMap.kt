1fc77ce4958ce8df388ace71fc63dc9f1e0fef91
Max <rudko.max10@gmail.com>
2020-08-24 13:59:01 +0300

    "Recursive" save for VetServiceMap.kt

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/map/VetServiceMap.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/map/VetServiceMap.kt
@@ -2,9 +2,21 @@ package com.justsoft.petclinic.services.map
 
 import com.justsoft.petclinic.models.people.Vet
 import com.justsoft.petclinic.services.VetService
+import com.justsoft.petclinic.services.VetSpecialtyService
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
 @Service
 @Profile("dev")
-class VetServiceMap: AbstractMapService<Vet>(), VetService
+class VetServiceMap(
+        val vetSpecialtyService: VetSpecialtyService
+) : AbstractMapService<Vet>(), VetService {
+
+    override fun save(anObject: Vet): Vet {
+        anObject.specialties.forEach { vetSpecialty ->
+            if (vetSpecialty.id == null)
+                vetSpecialtyService.save(vetSpecialty)
+        }
+        return super.save(anObject)
+    }
+}
