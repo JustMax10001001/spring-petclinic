0d44feceeefc0429b49a991436816e173d3ebde2
Max <rudko.max10@gmail.com>
2020-08-24 13:24:16 +0300

    Bootstrap refactoring and added PetType support. Closes #21

--- petclinic-web/src/main/kotlin/com/justsoft/petclinic/bootstrap/DataLoader.kt
+++ petclinic-web/src/main/kotlin/com/justsoft/petclinic/bootstrap/DataLoader.kt
@@ -2,8 +2,11 @@ package com.justsoft.petclinic.bootstrap
 
 import com.justsoft.petclinic.models.people.Owner
 import com.justsoft.petclinic.models.people.Vet
+import com.justsoft.petclinic.models.pets.Pet
+import com.justsoft.petclinic.models.pets.PetType
 import com.justsoft.petclinic.services.OwnerService
 import com.justsoft.petclinic.services.PetService
+import com.justsoft.petclinic.services.PetTypeService
 import com.justsoft.petclinic.services.VetService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
@@ -12,45 +15,76 @@ import org.springframework.stereotype.Component
 class DataLoader(
         private val ownerService: OwnerService,
         private val petService: PetService,
-        private val vetService: VetService
+        private val vetService: VetService,
+        private val petTypeService: PetTypeService,
 ) : CommandLineRunner {
 
 
     override fun run(vararg args: String?) {
-        ownerService.save(
-                Owner(
-                        firstName = "Michael",
-                        lastName = "Weston",
-                        address = "Kulparkivska",
-                        city = "Lviv",
-                        telephone = "0223352"
-                )
-        )
-        ownerService.save(
-                Owner(
-                        firstName = "Fiona",
-                        lastName = "Shrek",
-                        address = "Uzhgorodska",
-                        city = "Lviv",
-                        telephone = "0223351"
-                )
-        )
+        val dog = PetType("Dog")
+        val cat = PetType("Cat")
+        val racoon = PetType("Racoon")
+        savePetTypes(dog, cat, racoon)
+        println("Loaded pet types")
 
+        val ownerMichael = createOwner1()
+        val ownerFiona = createOwner2()
+        saveOwners(ownerMichael, ownerFiona)
         println("Loaded owners.")
 
-        vetService.save(
-                Vet(
-                        firstName = "Sam",
-                        lastName = "Axe"
-                )
-        )
-        vetService.save(
-                Vet(
-                        firstName = "Sam",
-                        lastName = "Hatchet"
-                )
-        )
-
+        val vetPick = createVet1()
+        val vetSam = createVet2()
+        saveVets(vetPick, vetSam)
         println("Loaded vets.")
     }
+
+    private fun savePets(vararg pets: Pet) {
+        pets.forEach {
+            petService.save(it)
+        }
+    }
+
+    private fun saveVets(vararg vets: Vet) {
+        vets.forEach {
+            vetService.save(it)
+        }
+    }
+
+    private fun savePetTypes(vararg petTypes: PetType) {
+        petTypes.forEach {
+            petTypeService.save(it)
+        }
+    }
+
+    private fun saveOwners(vararg owners: Owner) {
+        owners.forEach {
+            ownerService.save(it)
+        }
+    }
+
+    private fun createVet1(): Vet = Vet(
+            firstName = "Pick",
+            lastName = "Axe",
+    )
+
+    private fun createVet2(): Vet = Vet(
+            firstName = "Sam",
+            lastName = "Hatchet",
+    )
+
+    private fun createOwner1(): Owner = Owner(
+            firstName = "Michael",
+            lastName = "Weston",
+            address = "Kulparkivska",
+            city = "Lviv",
+            telephone = "0223352"
+    )
+
+    private fun createOwner2(): Owner = Owner(
+            firstName = "Fiona",
+            lastName = "Shrek",
+            address = "Uzhgorodska",
+            city = "Lviv",
+            telephone = "0223351"
+    )
 }
