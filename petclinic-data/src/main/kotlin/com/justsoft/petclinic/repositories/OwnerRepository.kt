573d787e611773fde8b1e9efad537f7ab3cdf8e3
Max <rudko.max10@gmail.com>
2020-08-27 00:03:44 +0300

    Added Owner JPA service, closes #25

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/repositories/OwnerRepository.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/repositories/OwnerRepository.kt
@@ -5,4 +5,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OwnerRepository: CrudRepository<Owner, Long>
+interface OwnerRepository: CrudRepository<Owner, Long> {
+
+    fun findOwnerByLastNameIgnoreCase(firstName: String): Owner?
+}
---
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/datajpa/OwnerServiceJPA.kt
@@ -0,0 +1,26 @@
+package com.justsoft.petclinic.services.datajpa
+
+import com.justsoft.petclinic.models.people.Owner
+import com.justsoft.petclinic.repositories.OwnerRepository
+import com.justsoft.petclinic.services.OwnerService
+import org.springframework.data.repository.findByIdOrNull
+
+class OwnerServiceJPA(
+        private val ownerRepository: OwnerRepository
+): OwnerService {
+
+    override fun findByLastName(lastName: String): Owner? = ownerRepository.findOwnerByLastNameIgnoreCase(lastName)
+
+    override fun findById(id: Long): Owner? = ownerRepository.findByIdOrNull(id)
+
+    override fun save(anObject: Owner): Owner = ownerRepository.save(anObject)
+
+    override fun findAll(): Set<Owner> =
+            HashSet<Owner>().apply {
+                ownerRepository.findAll().forEach { this.add(it) }
+            }
+
+    override fun delete(anObject: Owner) = ownerRepository.delete(anObject)
+
+    override fun deleteById(id: Long) = ownerRepository.deleteById(id)
+}
