f3a92ad740122cd6af00e68e6fa80a11264fd37d
Max <rudko.max10@gmail.com>
2020-08-26 20:50:33 +0300

    Convert Owner to a JPA Entity

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/people/Owner.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/people/Owner.kt
@@ -1,12 +1,17 @@
 package com.justsoft.petclinic.models.people
 
 import com.justsoft.petclinic.models.pets.Pet
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.OneToMany
 
+@Entity
 class Owner(
         firstName: String,
         lastName: String,
-        val pets: MutableSet<Pet> = HashSet(),
-        var address: String,
-        var city: String,
-        var telephone: String
+        @Column(nullable = false) var address: String,
+        @Column(nullable = false) var city: String,
+        @Column(nullable = false) var telephone: String,
+        @OneToMany
+        val pets: MutableSet<Pet> = HashSet()
 ): Person(firstName, lastName)
--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/people/Person.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/models/people/Person.kt
@@ -2,8 +2,10 @@ package com.justsoft.petclinic.models.people
 
 import com.justsoft.petclinic.models.BaseEntity
 import javax.persistence.Column
+import javax.persistence.MappedSuperclass
 
+@MappedSuperclass
 open class Person(
-        @Column var firstName: String,
-        @Column var lastName: String
+        @Column(nullable = false) var firstName: String,
+        @Column(nullable = false) var lastName: String
 ) : BaseEntity()
