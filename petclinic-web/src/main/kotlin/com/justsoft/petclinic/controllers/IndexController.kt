7e6b816aa65752e59ad4ee4a4969041b2b6a3241
Max <rudko.max10@gmail.com>
2020-08-26 20:46:41 +0300

    Getting webjars to work, fixing artifact id's of submodules

--- petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/IndexController.kt
+++ petclinic-web/src/main/kotlin/com/justsoft/petclinic/controllers/IndexController.kt
@@ -1,12 +1,11 @@
 package com.justsoft.petclinic.controllers
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class IndexController {
 
-    @RequestMapping("", "/", "index", "index.html")
+    //@RequestMapping("", "/", "index", "index.html")
     fun index(): String{
         return "index"
     }
