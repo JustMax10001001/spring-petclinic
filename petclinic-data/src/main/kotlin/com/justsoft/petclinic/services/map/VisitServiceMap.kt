1bbf73d0d197b83a3e71ddcd3fdee510a99b576d
Max <rudko.max10@gmail.com>
2020-09-28 20:26:41 +0300

    Add profile support

--- petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/map/VisitServiceMap.kt
+++ petclinic-data/src/main/kotlin/com/justsoft/petclinic/services/map/VisitServiceMap.kt
@@ -2,7 +2,9 @@ package com.justsoft.petclinic.services.map
 
 import com.justsoft.petclinic.models.Visit
 import com.justsoft.petclinic.services.VisitService
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
 @Service
+@Profile("dev")
 class VisitServiceMap: AbstractMapService<Visit>(), VisitService
